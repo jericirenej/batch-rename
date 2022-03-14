f254a0e29cde8ebeba1c4d04b021dec8947e5b2c
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-03-14 20:25:11 +0100

    Types update

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
 export type TransformTypes = "even" | "odd";
 export type ExtractBaseAndExtReturn = { baseName: string; ext: string }[];
 export type ExtractBaseAndExt = (fileList: string[]) => ExtractBaseAndExtReturn;
+export type RenameList = {rename:string, original: string}[];
+export type GenerateRenameList = (splitFileList: ExtractBaseAndExtReturn, transformPattern: TransformTypes) => RenameList;
