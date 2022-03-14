dd05f7b817146418b94634a96f1c01581b2a6f00
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-03-14 20:25:03 +0100

    Added cleanup option.

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ const program = new Command();
 program
   .description("Simple utility for batch renaming files using even or odd numbers")
   .option("--odd", "Rename using even numbers (2n)")
-  .option("--even", "Rename using odd numbers (2n)");
+  .option("--even", "Rename using odd numbers (2n)")
+  .option("--cleanRollback", "Remove rollback file, if it exists");
 
 program.parse();
 
 const options = program.opts();
-console.log(options);
+
 
 
