fda9e18a03ea57b8d437d3e41ab3cf4a7b8b1b2e
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-03-20 21:25:24 +0100

    SearchAndReplace fix (compare against complete original name, including extension).

--- src/converter/searchAndReplace.ts
+++ src/converter/searchAndReplace.ts
@@ -8,18 +8,19 @@ export const searchAndReplace: SearchAndReplace = (args) => {
   const generatedArgs = generateArguments(searchAndReplace!);
   const targetList = splitFileList.map((fileInfo) => {
     const { filter, replace } = generatedArgs;
-    let original = fileInfo.baseName,
-      rename = original;
+    let baseName = fileInfo.baseName,
+    rename = baseName;
     const ext = fileInfo.ext;
+    const original = `${baseName}${ext}`;
     if (filter && filter.test(original)) {
-      rename = fileInfo.baseName.replaceAll(filter, replace);
+      rename = original.replaceAll(filter, replace);
     }
-    return { original: `${original}${ext}`, rename: `${rename}${ext}` };
+    return { original, rename: `${rename}${ext}` };
   });
   return targetList;
 };
 
 const generateArguments: GenerateSearchAndReplaceArgs = (args) => {
   if (args.length === 1) return { filter: null, replace: args[0] };
-  return { filter: new RegExp(args[0],"g"), replace: args[1] };
+  return { filter: new RegExp(args[0], "g"), replace: args[1] };
 };
