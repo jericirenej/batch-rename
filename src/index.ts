88d65ebf274e76ede119aa3e2e6c0394ff8e0a2c
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-03-29 18:36:53 +0200

    Remove logging.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,7 @@ import type { OptionKeysWithValues } from "./types.js";
 
 (async () => {
   program.parse(process.argv);
-
   const options = program.opts() as OptionKeysWithValues;
-
-  // console.log(options);
-  // console.log(program);
   await parseOptions(options);
   process.exit(0);
 })();
