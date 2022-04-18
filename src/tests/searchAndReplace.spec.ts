947d9661cd419ba5ec17a23d4b2db70ea304f058
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-04-18 17:59:26 +0200

    - Tests written for searchAndReplace. - Updated test for generateArguments to include the "u" flag.

--- src/tests/searchAndReplace.spec.ts
+++ src/tests/searchAndReplace.spec.ts
@@ -1,5 +1,7 @@
 import * as regexTransform from "../converters/searchAndReplace.js";
 import * as utils from "../converters/utils.js";
+import { GenerateRenameListArgs } from "../types.js";
+import { generateMockSplitFileList } from "./mocks.js";
 
 const { extractBaseAndExt, truncateFile } = utils;
 
@@ -32,7 +34,7 @@ describe("generateArguments", () => {
   });
   it("Filter should have only global flag set", () => {
     const replaceConfig = generateArguments(exampleArgs);
-    expect(replaceConfig.filter!.flags).toBe("g");
+    expect(replaceConfig.filter!.flags).toBe("gu");
   });
 });
 
@@ -63,5 +65,110 @@ describe("optionalTruncate", () => {
     const response = optionalTruncate(truncate, modifiedName, sourcePath);
     const expected = `${modifiedName.slice(0, Number(truncate))}.ext`;
     expect(response).toBe(expected);
+  });
+});
+
+describe("searchAndReplace", () => {
+  const exampleArgs: GenerateRenameListArgs = {
+    splitFileList: generateMockSplitFileList(2),
+    searchAndReplace: ["baseName", "newName"],
+    truncate: undefined,
+    transformPattern: ["searchAndReplace"],
+  };
+  afterEach(() => jest.clearAllMocks());
+  it("Should return a properly shaped response", () => {
+    const response = searchAndReplace(exampleArgs);
+    expect(Array.isArray(response)).toBe(true);
+    response.forEach((entry) => {
+      const keys = Object.keys(entry) as (keyof typeof entry)[];
+      expect(keys).toEqual(["original", "rename", "sourcePath"]);
+      keys.forEach((key) => expect(typeof entry[key]).toBe("string"));
+    });
+  });
+  it("Should call generateArguments", () => {
+    searchAndReplace(exampleArgs);
+    expect(spyOnGenerateArguments).toHaveBeenCalledTimes(1);
+    expect(spyOnGenerateArguments).toHaveBeenCalledWith(
+      exampleArgs.searchAndReplace
+    );
+  });
+  it("Should replace text in provided file list", () => {
+    const response = searchAndReplace(exampleArgs);
+    const expectedExt = exampleArgs.splitFileList[0].ext;
+    response.forEach((entry, index) => {
+      expect(entry.rename).toBe(`newName${index + 1}${expectedExt}`);
+    });
+  });
+  it("Should replace all matches of a regex supplied filter in target file", () => {
+    const customMocksList = generateMockSplitFileList(2);
+    customMocksList[0].baseName = "extra-extra";
+    // Remove all 'ext' characters, except if preceded by string.
+    let filter = "(?<!\\.)ext";
+    const newArgs: GenerateRenameListArgs = {
+      ...exampleArgs,
+      searchAndReplace: [filter, "ult"],
+      splitFileList: customMocksList,
+    };
+    const response = searchAndReplace(newArgs);
+    expect(response[0].rename).toBe("ultra-ultra.ext");
+  });
+  it("Should return original name, if returned filter is empty", () => {
+    spyOnGenerateArguments.mockReturnValueOnce({
+      filter: null,
+      replace: "something",
+    });
+    const response = searchAndReplace(exampleArgs);
+    response.forEach((entry) => expect(entry.rename).toBe(entry.original));
+  });
+  it("Should return original name, if filter doesn't match", () => {
+    const customMocksList = generateMockSplitFileList(2);
+    customMocksList[0].baseName = "match";
+    customMocksList[1].baseName = "other";
+    const newArgs: GenerateRenameListArgs = {
+      ...exampleArgs,
+      splitFileList: customMocksList,
+      searchAndReplace: ["match", "replace"],
+    };
+    const response = searchAndReplace(newArgs);
+    expect(response[0].rename).toContain("replace");
+    expect(response[1].rename).toBe(response[1].original);
+  });
+  it("Should not call optionalTruncate, if truncate argument is falsy", () => {
+    searchAndReplace(exampleArgs);
+    expect(spyOnOptionalTruncate).not.toHaveBeenCalled();
+  });
+  it("Should call optionalTruncate with appropriate arguments, if truncate argument is supplied", () => {
+    const customMocksList = generateMockSplitFileList(2);
+    customMocksList.forEach((entry) => (entry.baseName = "match"));
+    const newArgs: GenerateRenameListArgs = {
+      ...exampleArgs,
+      splitFileList: customMocksList,
+      searchAndReplace: ["match", "replace"],
+      truncate: "5",
+    };
+    searchAndReplace(newArgs);
+    expect(spyOnOptionalTruncate).toHaveBeenCalledTimes(
+      exampleArgs.splitFileList.length
+    );
+    spyOnOptionalTruncate.mock.calls.forEach((callArgs, index) => {
+      const [rename, sourcePath] = [
+        `replace${customMocksList[index].ext}`,
+        customMocksList[index].sourcePath,
+      ];
+      expect(callArgs).toEqual([newArgs.truncate, rename, sourcePath]);
+    });
+  });
+  it("Should not call optionalTruncate for un-transformed elements", () => {
+    const customMocksList = generateMockSplitFileList(2);
+    customMocksList[0].baseName = "match";
+    customMocksList[1].baseName = "other";
+    const newArgs: GenerateRenameListArgs = {
+      ...exampleArgs,
+      splitFileList: customMocksList,
+      searchAndReplace: ["match", "replace"],
+      truncate: "5",
+    };
+    searchAndReplace(newArgs);
+    expect(spyOnOptionalTruncate).toHaveBeenCalledTimes(1);
   });
 });
